a0878424bd6d50317663ee25fb1665b6e97bae67
Kate Krasova <kate.krasova@icloud.com>
2023-10-10 23:00:39 +0400

    project added

---
+++ NewWeatherApp/NewWeatherApp/Modules/Weather/WeatherViewController.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherViewController.swift
+//  NewWeatherApp
+//
+//  Created by Kate on 10.10.2023.
+//
+
+import UIKit
+
+class WeatherViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+}
+
